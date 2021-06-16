46ddb3684a02fe1eaf6e0f0bd6cfa908c009a50d
Alessio Molinari <alessio@ilona.isti.cnr.it>
2021-06-16 10:09:47 +0200

    add embedding comp, bug fix and unit testing

--- preprocessing/preprocessing_embedding.py
+++ preprocessing/preprocessing_embedding.py
@@ -1,14 +1,17 @@
 from preprocessing.preprocessing_classification import clean_dataset
+from preprocessing.feature_extraction import tokenize_nopunct
 from functools import cached_property
 from tqdm import tqdm
 import numpy as np
+import pandas as pd
 import typing
 import pickle
 import os
 
 
 class Glove:
-    def __init__(self, glove_txt_prefix: str, dim=100):
+    unknown_tok = '[unk]'
+    def __init__(self, glove_txt_prefix='glove.twitter.27B.', dim=100):
         self.glove_txt_prefix = glove_txt_prefix
         self.dim = dim
 
@@ -23,9 +26,31 @@ class Glove:
             for line in tqdm(f):
                 data = line.split(' ')
                 embeddings[data[0]] = np.array([float(x) for x in data[1:]]) 
+        embeddings[self.unknown_tok] = np.vstack(list(embeddings.values())).mean(axis=0)
         with open(path + 'pkl', 'wb') as f:
             pickle.dump(embeddings, f)
         return embeddings
 
-    def tokens_embedding(self, tokens: typing.List[str]) -> typing.Optional[np.ndarray]:
-        return np.array(list(map(self.embeddings.get, tokens)))
+    def tokens_embedding(self, tokens: typing.List[str], reduction='mean') -> typing.Optional[np.ndarray]:
+        """
+        :param tokens: List of tokens to fetch
+        :param reduction: either 'mean' or 'sum', any other value will return the `token x emb_dim` matrix
+        """
+        emb = np.array(list(map(lambda t: self.embeddings.get(t, self.embeddings[self.unknown_tok]), tokens)))
+        if reduction == 'mean':
+            return emb.mean(axis=0)
+        elif reduction == 'sum':
+            return emb.sum(axis=0)
+        return emb
+
+
+def get_author_mean_embeddings(df: pd.DataFrame, glove: Glove) -> typing.Dict[str, np.ndarray]:
+    pass
+
+
+def get_lyrics_mean_embeddings(df: pd.DataFrame, glove: Glove) -> typing.Dict[str, np.ndarray]:
+    d = {}
+    for _, row in tqdm(df.iterrows()):
+        embeddings = glove.tokens_embedding(tokenize_nopunct(row.lyrics))
+        d[row.song] = embeddings
+    return d
