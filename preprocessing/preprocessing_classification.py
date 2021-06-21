d0181a71b7f66cdf5fec0dc839c706a71aff33a3
Silvia <silvia-cor@hotmail.com>
2021-06-21 09:23:14 +0200

    Binary classification

--- preprocessing/preprocessing_classification.py
+++ preprocessing/preprocessing_classification.py
@@ -1,14 +1,15 @@
-import numpy.random
 import pandas as pd
 import re
 import numpy as np
 import pickle
 import os
 import typing
+from datetime import datetime
+from tqdm import tqdm
 
 
 genres_acc = ['pop', 'progressive rock', 'rock', 'metal', 'country', 'rnb', 'funk', 'hip-hop', 'alternative',
-              'rap', 'disco', 'folk', 'jazz', 'blues', 'indie', 'christmas', 'soul', 'reggae', 'gospel', 'latin']
+              'rap', 'disco', 'folk', 'jazz', 'blues', 'indie', 'christmas', 'soul']
 
 
 def _get_genre(sub_genres):
@@ -19,12 +20,8 @@ def _get_genre(sub_genres):
             return genres_acc.index('metal')
         if sub_genre in ['electronic', 'dance', 'dancehall']:
             return genres_acc.index('disco')
-        if 'christian' in sub_genre:
-            return genres_acc.index('gospel')
         if 'hip hop' in sub_genre or 'hiphop' in sub_genre:
             return genres_acc.index('hip-hop')
-        if 'spanish' in sub_genre:
-            return genres_acc.index('latin')
         if sub_genre == 'r&b' or 'doo wop' in sub_genre:
             return genres_acc.index('rnb')
         for genre in genres_acc:
@@ -33,12 +30,44 @@ def _get_genre(sub_genres):
     return 'wtf'
 
 
-def _clean_lyrics(lyrics):
-    lyrics = re.sub(r'\[[^\[]*?\]', '', lyrics)  # remove things like [Chorus] [First verse]
-    lyrics = re.sub(r'\{[^\{]*?\}', '', lyrics)  # remove things like [Chorus] [First verse]
-    lyrics = re.sub(r'\r?\n|\r', '\n', lyrics)  # transform white lines in \n
-    lyrics = re.sub(r'\n+', '\n', lyrics)  # remove duplicated \n
+remove_chorus = re.compile(r'\[[^\[]*?\]')
+white_trans = re.compile(r'\r?\n|\r')
+duplicate_rem = re.compile(r'\n+')
+
+
+def _clean_lyrics(lyrics: str):
+    lyrics = remove_chorus.sub('', lyrics)  # remove things like [Chorus] [First verse]
+    lyrics = white_trans.sub('\n', lyrics)  # transform white lines in \n
+    lyrics = duplicate_rem.sub('\n', lyrics)  # remove duplicated \n
     return lyrics
+
+
+def __mean_rank_join(df_to_group: pd.DataFrame, df_to_join: pd.DataFrame, num_weeks: float, mean_name: str, popul_name: str):
+    group = df_to_group.groupby('song').agg({'rank': 'sum', 'weeks-on-board': 'max'})
+    group[mean_name] = (group['rank'] + (101 * (num_weeks - group['weeks-on-board']))) / num_weeks
+    group[popul_name] = (101 - group['rank']) * group['weeks-on-board']
+    return df_to_join.join(group.drop(columns=['rank', 'weeks-on-board']), 'song')
+
+
+def add_chart_info(df: pd.DataFrame, chart_path: str) -> pd.DataFrame:
+    assert not (df.duplicated('song', keep=False)).any(), 'df should not have duplicated songs'
+    charts = pd.read_csv(chart_path)
+    charts.date = pd.to_datetime(charts.date, format='%Y-%m-%d')
+    df.date = pd.to_datetime(df.date, format='%Y-%m-%d')
+    # Consider all dates before 1960 as 1960 and all dates after 2020 as 2020
+    charts.date = charts.date.apply(lambda d: datetime(year=1960, month=d.month, day=d.day) if d.year < 1960 else d)
+    charts.date = charts.date.apply(lambda d: datetime(year=2020, month=d.month, day=d.day) if d.year > 2020 else d)
+
+    total_num_weeks = abs((df.iloc[0].date - df.iloc[-1].date).days / 7)
+    df = __mean_rank_join(charts, df, total_num_weeks, 'rank_alltime', 'popul_alltime')
+
+    # Group dates by decades
+    decades = charts.groupby(pd.Grouper(key='date', freq='10YS'))
+    for dt, decade in tqdm(decades, desc='Computing decades rank'):
+        n_weeks = abs((decade.iloc[0].date - decade.iloc[-1].date).days / 7)
+        df = __mean_rank_join(decade, df, n_weeks, f'rank_{dt.year}', f'popul_{dt.year}')
+
+    return df
 
 
 def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
@@ -46,12 +75,11 @@ def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
     df = df[df.genre.notna()]
     df = df[df.lyrics.str.split().str.len() <= 2000]  # remove books(?)
     df = df[df.lyrics.str.split().str.len() > 5]  # remove almost-empty strings
-    df = df[df.groupby('artist').artist.transform('count') > 10]  # leaves only artists with n+ songs
     df = df[df['artist'] != 'Glee Cast']  # bravi ma non orginali
     df['lyrics'] = df['lyrics'].apply(_clean_lyrics)
     df['genre'] = df['genre'].apply(_get_genre)
     df = df[df.genre != 'wtf']
-    return df
+    return df[df.groupby('artist').artist.transform('count') >= 20]  # leaves only artists with n+ songs
 
 
 def df_as_dict(df: pd.DataFrame) -> typing.Dict[str, np.ndarray]:
@@ -63,29 +91,26 @@ def df_as_dict(df: pd.DataFrame) -> typing.Dict[str, np.ndarray]:
     return dataset
 
 
-def select_random_authors(df, n_authors):
+def select_random_authors(df, n_authors, seed):
     authors = np.unique(df['artist'])
-    np.random.seed(42)
+    np.random.seed(seed)
     selected_authors = np.random.choice(authors, n_authors, replace=False)
     df = df.loc[df['artist'].isin(selected_authors)]
     return df
 
 
-def fetch_dataset(pickle_path, lyrics_path, force=False, as_dict=False, random_authors=0):
-    if os.path.exists(pickle_path) and not force:
+def fetch_dataset(pickle_path, lyrics_path, force=False, as_dict=False, random_authors=0, seed=42) -> typing.Union[pd.DataFrame, typing.Dict[str, np.ndarray]]:
+    if pickle_path is not None and os.path.exists(pickle_path) and not force:
         with open(pickle_path, 'rb') as f:
             dataset = pickle.load(f)
     else:
-        try:
-            os.remove(pickle_path)
-        except OSError:
-            pass
         df = pd.read_csv(lyrics_path)
         dataset = clean_dataset(df)
+        dataset = add_chart_info(dataset, 'data/charts.csv')
         with open(pickle_path, 'wb') as f:
             pickle.dump(dataset, f)
     if random_authors != 0:
-        dataset = select_random_authors(dataset, random_authors)
+        dataset = select_random_authors(dataset, random_authors, seed)
     if as_dict:
         dataset = df_as_dict(dataset)
     return dataset
---
+++ preprocessing/test_preprocessing.py
@@ -0,0 +1,27 @@
+import unittest
+from preprocessing import *
+
+
+class TestPreprocessing(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read_csv('../data/results_genre.csv')
+
+    def test_add_chart_info(self):
+        df = add_chart_info(self.df, '../data/charts.csv')
+        charts = pd.read_csv('../data/charts.csv')
+        charts.date = pd.to_datetime(charts.date, format='%Y-%m-%d')
+        test_song = "Poor Little Fool"
+        test_rank = charts[charts.song == test_song]
+        test_rank_sum = test_rank['rank'].sum()
+        total_num_weeks = abs((charts.iloc[0].date - charts.iloc[-1].date).days / 7)
+        test_rank = (test_rank_sum + (101 * (total_num_weeks - len(test_rank)))) / total_num_weeks
+        self.assertEqual(df[df.song == test_song]['rank_alltime'][0], test_rank)
+
+    def test_songs_for_author(self):
+        df = clean_dataset(self.df)
+        artists = df.drop_duplicates('artist')
+        self.assertTrue(all((df.artist == artist).sum() > 10 for artist in artists.artist))
+
+
+if __name__ == '__main__':
+    unittest.main()
